1f1d665b5cd1a85a37ab280ea70f8a7f83c65ab5
wowgod9504 <mike1124@naver.com>
2020-06-20 23:36:43 +0900

    add videoBlock mixin html structure & testdb

---
+++ db.js
@@ -0,0 +1,51 @@
+export const videos = [
+    {
+        id : 323410,
+        title : 'Video awesome',
+        description : 'This is something I love',
+        views : 24,
+        videoFile : "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator : {
+            id : 123123,
+            name : "Noah",
+            email : 'mike1124@naver.com'
+        }
+    },
+    {
+        id : 123241,
+        title : 'Video Super',
+        description : 'This is something I love',
+        views : 24,
+        videoFile : "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator : {
+            id : 123123,
+            name : "Noah",
+            email : 'mike1124@naver.com'
+        }
+    },
+    {
+        id : 325262,
+        title : 'Video nice',
+        description : 'This is something I love',
+        views : 24,
+        videoFile : "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator : {
+            id : 123123,
+            name : "Noah",
+            email : 'mike1124@naver.com'
+        }
+    },
+    {
+        id : 121322,
+        title : 'Video perfect',
+        description : 'This is something I love',
+        views : 24,
+        videoFile : "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator : {
+            id : 123123,
+            name : "Noah",
+            email : 'mike1124@naver.com'
+        }
+    }
+]
+
